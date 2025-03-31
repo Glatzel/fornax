a344af53227a7e514bc7b8869d962dceb42bd2fe
Glatzel <893016099@qq.com>
2025-03-31 15:20:33 +0800

    fix

--- crates/libraw-sys/src/lib.rs
+++ crates/libraw-sys/src/lib.rs
@@ -1,6 +1,15 @@
-#[cxx::bridge]
-mod ffi {
-    unsafe extern "C++" {
-        include!("../../vcpkg/installed/x64-windows-static-md/include/libraw/libraw.h");
-    }
-}
+#![allow(
+    non_camel_case_types,
+    non_upper_case_globals,
+    clippy::approx_constant,
+    clippy::redundant_static_lifetimes,
+    non_snake_case
+)]
+#![no_std]
+
+#[cfg(feature = "bindgen")]
+include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+#[cfg(not(feature = "bindgen"))]
+mod bindings;
+#[cfg(not(feature = "bindgen"))]
+pub use bindings::*;
