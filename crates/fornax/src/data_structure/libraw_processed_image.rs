9bd45efda36401328c2c1c519f599e4b9a24ee45
Glatzel <893016099@qq.com>
2025-04-02 19:51:46 +0800

    fix

--- crates/fornax/src/data_structure/libraw_processed_image.rs
+++ crates/fornax/src/data_structure/libraw_processed_image.rs
@@ -3,31 +3,36 @@ pub enum ImageFormats {
     LibrawImageJpeg = 2,
 }
 pub struct LibrawProcessedImage {
-    pub image_type: ImageFormats,
-    pub height: u16,
-    pub width: u16,
-    pub colors: u16,
-    pub bis: u16,
-    pub data_size: u32,
-    pub data: [u8; 1],
+    pub ptr: *mut libraw_sys::libraw_processed_image_t,
 }
 impl LibrawProcessedImage {
     pub(crate) fn new(
-        processed_image: &libraw_sys::libraw_processed_image_t,
+        ptr: *mut libraw_sys::libraw_processed_image_t,
     ) -> miette::Result<LibrawProcessedImage> {
-        let img: LibrawProcessedImage = Self {
-            image_type: match processed_image.type_ {
-                1 => ImageFormats::ImageBitmap,
-                2 => ImageFormats::LibrawImageJpeg,
-                t => miette::bail!("Unknow processed_image type: {t}"),
-            },
-            height: processed_image.height,
-            width: processed_image.width,
-            colors: processed_image.colors,
-            bis: processed_image.bits,
-            data_size: processed_image.data_size,
-            data: processed_image.data,
-        };
+        clerk::debug!("Is processed image null: {}", ptr.is_null());
+        let img: LibrawProcessedImage = Self { ptr };
         Ok(img)
+    }
+    pub fn image_type(&self) -> miette::Result<ImageFormats> {
+        match unsafe { (*self.ptr).type_ } {
+            1i32 => Ok(ImageFormats::ImageBitmap),
+            2i32 => Ok(ImageFormats::LibrawImageJpeg),
+            t => miette::bail!("Unknow image format: {t}"),
+        }
+    }
+    pub fn height(&self) -> u16 {
+        unsafe { (*self.ptr).width }
+    }
+    pub fn width(&self) -> u16 {
+        unsafe { (*self.ptr).width }
+    }
+    pub fn colors(&self) -> u16 {
+        unsafe { (*self.ptr).colors }
+    }
+    pub fn bits(&self) -> u16 {
+        unsafe { (*self.ptr).bits }
+    }
+    pub fn data_size(&self) -> u32 {
+        unsafe { (*self.ptr).data_size }
     }
 }
