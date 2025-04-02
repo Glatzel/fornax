512e1e77c4e3caab9390148dda0bfe9c0374d78b
Glatzel <893016099@qq.com>
2025-04-02 19:38:50 +0800

    fix

--- crates/fornax/src/data_structure/libraw_image_sizes.rs
+++ crates/fornax/src/data_structure/libraw_image_sizes.rs
@@ -24,34 +24,17 @@ impl From<i32> for Flip {
 #[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
 #[derive(Debug, Copy, Clone)]
 pub struct LibrawImageSizes {
-    ///Full size of RAW image (including the frame) in pixels.
-    pub raw_height: u16,
-    /// Full size of RAW image (including the frame) in pixels.
-    pub raw_width: u16,
-    ///Size of visible ("meaningful") part of the image (without the frame).
-    pub height: u16,
-    ///Size of visible ("meaningful") part of the image (without the frame).
-    pub width: u16,
-    ///Coordinates of the top left corner of the frame (the second corner is calculated from the
-    /// full size of the image and size of its visible part).
-    pub top_margin: u16,
-    ///Coordinates of the top left corner of the frame (the second corner is calculated from the
-    /// full size of the image and size of its visible part).
-    pub left_margin: u16,
-    ///Size of the output image (may differ from height/width for cameras that require image
-    /// rotation or have non-square pixels).
-    pub iheight: u16,
-    ///Size of the output image (may differ from height/width for cameras that require image
-    /// rotation or have non-square pixels).
-    pub iwidth: u16,
-    ///Full size of raw data row in bytes .
-    pub raw_pitch: u32,
-    /// Pixel width/height ratio. If it is not unity, scaling of the image along one of the axes is
-    /// required during output.
-    pub pixel_aspect: f64,
-    ///Image orientation (0 if does not require rotation; 3 if requires 180-deg rotation; 5 if 90
-    /// deg counterclockwise, 6 if 90 deg clockwise).
-    pub flip: Flip,
+    raw_height: u16,
+    raw_width: u16,
+    height: u16,
+    width: u16,
+    top_margin: u16,
+    left_margin: u16,
+    iheight: u16,
+    iwidth: u16,
+    raw_pitch: u32,
+    pixel_aspect: f64,
+    flip: Flip,
 }
 
 impl LibrawImageSizes {
@@ -69,5 +52,55 @@ impl LibrawImageSizes {
             pixel_aspect: imgdata.sizes.pixel_aspect,
             flip: Flip::from(imgdata.sizes.flip),
         }
+    }
+    ///Full size of RAW image (including the frame) in pixels.
+    pub fn raw_height(&self) -> u16 {
+        self.raw_height
+    }
+    /// Full size of RAW image (including the frame) in pixels.
+    pub fn raw_width(&self) -> u16 {
+        self.raw_width
+    }
+    ///Size of visible ("meaningful") part of the image (without the frame).
+    pub fn height(&self) -> u16 {
+        self.height
+    }
+    ///Size of visible ("meaningful") part of the image (without the frame).
+    pub fn width(&self) -> u16 {
+        self.width
+    }
+    ///Coordinates of the top left corner of the frame (the second corner is calculated from the
+    /// full size of the image and size of its visible part).
+    pub fn top_margin(&self) -> u16 {
+        self.top_margin
+    }
+    ///Coordinates of the top left corner of the frame (the second corner is calculated from the
+    /// full size of the image and size of its visible part).
+    pub fn left_margin(&self) -> u16 {
+        self.left_margin
+    }
+    ///Size of the output image (may differ from height/width for cameras that require image
+    /// rotation or have non-square pixels).
+    pub fn iheight(&self) -> u16 {
+        self.iheight
+    }
+    ///Size of the output image (may differ from height/width for cameras that require image
+    /// rotation or have non-square pixels).
+    pub fn iwidth(&self) -> u16 {
+        self.iwidth
+    }
+    ///Full size of raw data row in bytes .
+    pub fn raw_pitch(&self) -> u32 {
+        self.raw_pitch
+    }
+    /// Pixel width/height ratio. If it is not unity, scaling of the image along one of the axes is
+    /// required during output.
+    pub fn pixel_aspect(&self) -> f64 {
+        self.pixel_aspect
+    }
+    ///Image orientation (0 if does not require rotation; 3 if requires 180-deg rotation; 5 if 90
+    /// deg counterclockwise, 6 if 90 deg clockwise).
+    pub fn flip(&self) -> Flip {
+        self.flip
     }
 }
