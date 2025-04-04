fe75ca4bb6f36bf02593960101f817480bf7ad87
Glatzel <893016099@qq.com>
2025-04-04 09:31:18 +0800

    fix

--- scripts/examples.ps1
+++ scripts/examples.ps1
@@ -4,5 +4,6 @@ Set-Location $PSScriptRoot/..
 
 New-Item temp -ItemType Directory -ErrorAction SilentlyContinue
 
+git submodule update --init --recursive
 cargo run --package fornax --example "process"
 cargo run --package fornax --example "sizes"
