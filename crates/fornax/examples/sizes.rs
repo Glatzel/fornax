d4371ee93b9d6414a3525915658789415f3bcefe
Glatzel <893016099@qq.com>
2025-04-04 10:45:32 +0800

    fix

--- crates/fornax/examples/sizes.rs
+++ crates/fornax/examples/sizes.rs
@@ -1,7 +1,6 @@
 use std::path::PathBuf;
 
 use fornax::Fornax;
-use miette::IntoDiagnostic;
 use tracing::level_filters::LevelFilter;
 use tracing_subscriber::layer::SubscriberExt;
 use tracing_subscriber::util::SubscriberInitExt;
@@ -9,7 +8,7 @@ fn main() -> miette::Result<()> {
     tracing_subscriber::registry()
         .with(clerk::terminal_layer(LevelFilter::DEBUG))
         .init();
-    let mut processor = Fornax::new();
+    let processor = Fornax::new();
     processor
         .open_file(PathBuf::from(
             "./external/raw-images/images/colorchart-5D2-6000K.dng",
--- crates/fornax/src/utils.rs
+++ crates/fornax/src/utils.rs
@@ -1,3 +1,3 @@
 pub fn mnt_to_string(bytes: &[i8]) -> String {
-   unsafe { std::str::from_utf8_unchecked(std::mem::transmute(bytes)) }.to_string()
+    unsafe { std::str::from_utf8_unchecked(std::mem::transmute::<&[i8], &[u8]>(bytes)) }.to_string()
 }
