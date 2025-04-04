ed162b347b4e130ac3c94f248b6567419498bfc3
Glatzel <893016099@qq.com>
2025-04-04 12:27:04 +0800

    fix

--- crates/fornax/src/iparams.rs
+++ crates/fornax/src/iparams.rs
@@ -52,51 +52,83 @@ impl IParams {
             },
         })
     }
+    ///Camera manufacturer.
     pub fn make(&self) -> String {
         self.make.clone()
     }
+    ///Camera model.
     pub fn model(&self) -> String {
         self.model.clone()
     }
+    ///There is a huge number of identical cameras sold under different names, depending on the
+    /// market (e.g. multiple Panasonic or Canon models) and even some identical cameras sold under
+    /// different brands (Panasonic -> Leica, Sony -> Hasselblad). normalized_make contains primary
+    /// vendor name (e.g. Panasonic for Leica re-branded cameras).
     pub fn normalized_make(&self) -> String {
         self.normalized_make.clone()
     }
+    ///Primary camera model name.
     pub fn normalized_model(&self) -> String {
         self.normalized_model.clone()
     }
+    ///Primary vendor name in indexed form (enum LibRaw_cameramaker_index, LIBRAW_CAMERAMAKER_*
+    /// constant)
     pub fn maker_index(&self) -> u32 {
         self.maker_index
     }
+    ///Softwary name/version (mostly for DNG files, to distinguish in-camera DNGs from Adobe DNG
+    /// Converter produced ones).
     pub fn software(&self) -> String {
         self.software.clone()
     }
+    ///   Number of RAW images in file (0 means that the file has not been recognized).
     pub fn raw_count(&self) -> u32 {
         self.raw_count
     }
+    ///Nonzero for Sigma Foveon images
     pub fn is_foveon(&self) -> bool {
         self.is_foveon
     }
+    ///DNG version (for the DNG format).
     pub fn dng_version(&self) -> u32 {
         self.dng_version
     }
+    ///  Number of colors in the file.
     pub fn colors(&self) -> i32 {
         self.colors
     }
+    ///Bit mask describing the order of color pixels in the matrix (0 for full-color images). 32
+    /// bits of this field describe 16 pixels (8 rows with two pixels in each, from left to right
+    /// and from top to bottom). Each two bits have values 0 to 3, which correspond to four possible
+    /// colors. Convenient work with this field is ensured by the COLOR(row,column) function, which
+    /// returns the number of the active color for a given pixel.
+    ///
+    ///Values less than 1000 are reserved as special cases:
+    ///- 1 - Leaf Catchlight with 16x16 bayer matrix;
+    ///- 9 - Fuji X-Trans (6x6 matrix)
+    ///- 3..8 and 10..999 - are unused.
     pub fn filters(&self) -> u32 {
         self.filters
     }
+    ///These matrices contains Fuji X-Trans row/col to color mapping. First one is relative to
+    /// visible area, while second is positioned relative to sensor edges.
     pub fn xtrans(&self) -> [[i8; 6]; 6] {
         self.xtrans
     }
+    ///These matrices contains Fuji X-Trans row/col to color mapping. First one is relative to
+    /// visible area, while second is positioned relative to sensor edges.
     pub fn xtrans_abs(&self) -> [[i8; 6]; 6] {
         self.xtrans_abs
     }
+    ///Description of colors numbered from 0 to 3 (RGBG,RGBE,GMCY, or GBTG).
     pub fn cdesc(&self) -> String {
         self.cdesc.clone()
     }
+    ///XMP packed data length and pointer to extracted XMP packet.
     pub fn xmplen(&self) -> u32 {
         self.xmplen
     }
+    ///XMP packed data length and pointer to extracted XMP packet.
     pub fn xmpdata(&self) -> String {
         self.xmpdata.clone()
     }
--- crates/fornax/src/processed_image.rs
+++ crates/fornax/src/processed_image.rs
@@ -2,8 +2,8 @@ use core::slice;
 use std::fmt::Display;
 #[derive(Debug)]
 pub enum ImageFormats {
-    ImageBitmap = 1,
-    LibrawImageJpeg = 2,
+    LibrawImageJpeg = 1,
+    ImageBitmap = 2,
 }
 impl Display for ImageFormats {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
@@ -26,27 +26,47 @@ impl ProcessedImage {
         };
         Ok(img)
     }
+    /// This field records type of data, containing in remaining fields of structure.
+    /// - LIBRAW_IMAGE_BITMAP - structure contains RGB bitmap. All metadata fields (see below) are
+    ///   valid and describes image data.
+    /// - LIBRAW_IMAGE_JPEG - structure contain in-memory image of JPEG file. Only type, data_size
+    ///   and data fields are valid (and nonzero);
     pub fn image_type(&self) -> miette::Result<ImageFormats> {
         match unsafe { (*self.processed_image).type_ } {
-            1i32 => Ok(ImageFormats::ImageBitmap),
-            2i32 => Ok(ImageFormats::LibrawImageJpeg),
+            1i32 => Ok(ImageFormats::LibrawImageJpeg),
+            2i32 => Ok(ImageFormats::ImageBitmap),
             t => miette::bail!("Unknow image format: {t}"),
         }
     }
+    /// Image size (in pixels). Valid only if type==LIBRAW_IMAGE_BITMAP.
     pub fn height(&self) -> u16 {
         unsafe { (*self.processed_image).height }
     }
+    /// Image size (in pixels). Valid only if type==LIBRAW_IMAGE_BITMAP.
     pub fn width(&self) -> u16 {
         unsafe { (*self.processed_image).width }
     }
+    /// Number of colors components (1 or 3) and color depth in bits (8 or 16). These fields are
+    /// valid only if type==LIBRAW_IMAGE_BITMAP.
     pub fn colors(&self) -> u16 {
         unsafe { (*self.processed_image).colors }
     }
+    ///Is bitmap data gamma-corrected (always 1 for 8-bit data, may be 0 or 1 for 16-bit). Valid
+    /// only if type==LIBRAW_IMAGE_BITMAP.
     pub fn bits(&self) -> u16 {
         unsafe { (*self.processed_image).bits }
     }
+    ///Size of data field (in bytes). For bitmap image equal to (height*width*colors * (bits/8)).
+    /// For JPEG image - exact JPEG size (i.e. extracted thnumbnail size + JPEG header + EXIF
+    /// header).
     pub fn data_size(&self) -> u32 {
         unsafe { (*self.processed_image).data_size }
+    }
+    ///Size of data field (in bytes). For bitmap image equal to (height*width*colors * (bits/8)).
+    /// For JPEG image - exact JPEG size (i.e. extracted thnumbnail size + JPEG header + EXIF
+    /// header).
+    pub fn data(&self) -> *const u8 {
+        unsafe { (*self.processed_image).data.as_ptr() }
     }
 }
 impl Drop for ProcessedImage {
