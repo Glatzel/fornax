1c57c495124235a862f3180dd0f757827cc786b7
Glatzel <893016099@qq.com>
2025-04-04 20:13:08 +0800

    fix

--- crates/fornax/src/presets.rs
+++ crates/fornax/src/presets.rs
@@ -1,8 +1,9 @@
-pub trait PresetCg {
-    fn preset_cg(&mut self) -> &mut Self;
-}
-impl PresetCg for crate::OutputParams {
-    fn preset_cg(&mut self) -> &mut Self {
+impl crate::OutputParams {
+    /// Match output to cg workflow.
+    /// - `gamm` = `[1.0, 1.0]`
+    /// - `output_color`: ACES
+    /// - `output_bps`: 16bit
+    pub fn preset_cg(&mut self) -> &mut Self {
         self.gamm = Some([1.0, 1.0]);
         self.output_color = Some(fornax::OutputColor::ACES);
         self.output_bps = Some(fornax::OutputBps::_16bit);
