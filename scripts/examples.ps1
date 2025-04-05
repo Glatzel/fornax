f45e2a29b9ce5a2fb88eb68844341b1134d1ce5b
Glatzel <893016099@qq.com>
2025-04-05 14:41:02 +0800

    fix

--- scripts/examples.ps1
+++ scripts/examples.ps1
@@ -4,8 +4,8 @@ Set-Location $PSScriptRoot/..
 New-Item temp -ItemType Directory -ErrorAction SilentlyContinue
 
 git submodule update --init --recursive
-# cargo run --package fornax --example "imgother"
-# cargo run --package fornax --example "iparams"
+cargo run --package fornax --example "imgother"
+cargo run --package fornax --example "iparams"
 cargo run --package fornax --example "process-cg"
 cargo run --package fornax --example "process"
-# cargo run --package fornax --example "sizes"
+cargo run --package fornax --example "sizes"
