2314ab371d76ba9cd76f29fa3be15970a6ed991d
Glatzel <chenzelin9794@gmail.com>
2025-04-05 18:02:42 +0800

    feat: isolate dcraw (#6)
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

---
+++ crates/dcraw/src/lib.rs
@@ -0,0 +1,70 @@
+mod output_params;
+mod processed_image;
+use fornax_core::{IDecoder, IPostProcessor, ProcessedImage};
+use libraw::{ILibraw, Libraw};
+pub use output_params::{
+    DCRawFbddNoiserd, DCRawHighlightMode, DCRawOutputBps, DCRawOutputColor, DCRawParams,
+    DCRawUserFlip, DCRawUserQual,
+};
+pub use processed_image::{DcRawProcessedImage, ImageFormats};
+#[derive(Default)]
+pub struct DCRaw {
+    pub(crate) params: Option<DCRawParams>,
+}
+impl DCRaw {
+    pub fn new(params: DCRawParams) -> Self {
+        Self {
+            params: Some(params),
+        }
+    }
+}
+
+impl DCRaw {
+    fn set_output_params_unsafe(
+        &self,
+        imgdata: *mut libraw_sys::libraw_data_t,
+    ) -> miette::Result<()> {
+        if let Some(params) = &self.params {
+            params.set_output_params(imgdata)?;
+        }
+        clerk::debug!("Set new params.");
+        clerk::debug!("{:?}", unsafe { (*imgdata).params });
+        Ok(())
+    }
+    fn dcraw_process_unsafe(
+        &self,
+        imgdata: *mut libraw_sys::libraw_data_t,
+    ) -> miette::Result<DcRawProcessedImage> {
+        self.set_output_params_unsafe(imgdata)?;
+
+        Libraw::check_run(unsafe { libraw_sys::libraw_dcraw_process(imgdata) })?;
+        let mut result = 0i32;
+        let processed: *mut libraw_sys::libraw_processed_image_t =
+            unsafe { libraw_sys::libraw_dcraw_make_mem_image(imgdata, &mut result) };
+        Libraw::check_run(result)?;
+
+        let processed = DcRawProcessedImage::new(processed)?;
+        Ok(processed)
+    }
+}
+impl DCRaw {
+    pub fn set_output_params(&self, imgdata: *mut libraw_sys::libraw_data_t) -> miette::Result<()> {
+        self.set_output_params_unsafe(imgdata)
+    }
+    pub fn dcraw_process(
+        &self,
+        imgdata: *mut libraw_sys::libraw_data_t,
+    ) -> miette::Result<DcRawProcessedImage> {
+        self.dcraw_process_unsafe(imgdata)
+    }
+}
+impl<T> IPostProcessor<T, ProcessedImage> for DCRaw
+where
+    T: ILibraw + IDecoder,
+{
+    fn post_process(&self, libraw: &T) -> miette::Result<ProcessedImage> {
+        let imgdata = libraw.imgdata()?;
+        let processed = self.dcraw_process_unsafe(imgdata)?.to_image()?;
+        Ok(processed)
+    }
+}
