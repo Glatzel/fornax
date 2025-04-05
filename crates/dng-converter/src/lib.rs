616ad1704ab853c5e108d426e116be687a86fd5c
Glatzel <893016099@qq.com>
2025-04-05 17:37:16 +0800

    fix

--- crates/dng-converter/src/lib.rs
+++ crates/dng-converter/src/lib.rs
@@ -1,7 +1,48 @@
 use libraw::ILibraw;
+use miette::IntoDiagnostic;
+use sha2::{Digest, Sha256};
+use std::{io::Read, path::PathBuf};
 
 pub struct DngConverter {
     imgdata: Option<*mut libraw_sys::libraw_data_t>,
+    raw_file: PathBuf,
+    hash: String,
+}
+impl DngConverter {
+    pub fn new(raw_file: PathBuf) -> miette::Result<Self> {
+        let hash = DngConverter::calculate_hash(&raw_file)?;
+        Ok(Self {
+            imgdata: None,
+            raw_file: raw_file.into(),
+            hash,
+        })
+    }
+    fn calculate_hash(raw_file: &PathBuf) -> miette::Result<String> {
+        let mut file = std::fs::File::open(raw_file).into_diagnostic()?;
+        let mut hasher = Sha256::new();
+
+        // Buffer to read the file in chunks
+        let mut buffer = [0u8; 4096];
+        loop {
+            let bytes_read = file.read(&mut buffer).into_diagnostic()?;
+            if bytes_read == 0 {
+                break; // End of file reached
+            }
+            hasher.update(&buffer[..bytes_read]);
+        }
+
+        // Finalize and get the hash result
+        let result = hasher.finalize();
+
+        // Convert the result to a hexadecimal string
+        Ok(format!("{:x}", result))
+    }
+    pub fn raw_file(&self) -> &PathBuf {
+        &self.raw_file
+    }
+    pub fn hash(&self) -> &str {
+        &self.hash
+    }
 }
 impl fornax_core::IDecoder for DngConverter {
     fn decode_file(&mut self, file: std::path::PathBuf) -> miette::Result<()> {
