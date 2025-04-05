a183fdc78823dcdbb77d5d0a3144caa751c65019
Glatzel <893016099@qq.com>
2025-04-05 10:42:44 +0800

    fix

--- crates/fornax/examples/iparams.rs
+++ crates/fornax/examples/iparams.rs
@@ -1,21 +1,20 @@
-// use std::path::PathBuf;
+use std::path::PathBuf;
 
-// use fornax::Fornax;
-// use tracing::level_filters::LevelFilter;
-// use tracing_subscriber::layer::SubscriberExt;
-// use tracing_subscriber::util::SubscriberInitExt;
-// fn main() -> miette::Result<()> {
-//     tracing_subscriber::registry()
-//         .with(clerk::terminal_layer(LevelFilter::DEBUG))
-//         .init();
-//     let processor = Fornax::new();
-//     processor.open_file(PathBuf::from(
-//         "./external/raw-images/images/colorchart-5D2-6000K.dng",
-//     ))?;
-//     processor.unpack().unwrap();
+use fornax::Fornax;
+use tracing::level_filters::LevelFilter;
+use tracing_subscriber::layer::SubscriberExt;
+use tracing_subscriber::util::SubscriberInitExt;
+fn main() -> miette::Result<()> {
+    tracing_subscriber::registry()
+        .with(clerk::terminal_layer(LevelFilter::DEBUG))
+        .init();
+    let mut libraw = libraw::Libraw::new();
+    let mut manager = Fornax::new(&libraw, &libraw);
+    manager.decode_file(PathBuf::from(
+        "./external/raw-images/images/colorchart-5D2-6000K.dng",
+    ))?;
 
-//     let iparams = processor.iparams()?;
-//     println!("{:?}", iparams);
-//     Ok(())
-// }
-fn main(){}
+    let iparams = libraw.iparams()?;
+    println!("{:?}", iparams);
+    Ok(())
+}
--- crates/fornax/examples/sizes.rs
+++ crates/fornax/examples/sizes.rs
@@ -1,21 +1,20 @@
-// use std::path::PathBuf;
+use std::path::PathBuf;
 
-// use fornax::Fornax;
-// use tracing::level_filters::LevelFilter;
-// use tracing_subscriber::layer::SubscriberExt;
-// use tracing_subscriber::util::SubscriberInitExt;
-// fn main() -> miette::Result<()> {
-//     tracing_subscriber::registry()
-//         .with(clerk::terminal_layer(LevelFilter::DEBUG))
-//         .init();
-//     let processor = Fornax::new();
-//     processor.open_file(PathBuf::from(
-//         "./external/raw-images/images/colorchart-5D2-6000K.dng",
-//     ))?;
-//     processor.unpack().unwrap();
+use fornax::Fornax;
+use tracing::level_filters::LevelFilter;
+use tracing_subscriber::layer::SubscriberExt;
+use tracing_subscriber::util::SubscriberInitExt;
+fn main() -> miette::Result<()> {
+    tracing_subscriber::registry()
+        .with(clerk::terminal_layer(LevelFilter::DEBUG))
+        .init();
+    let mut libraw = libraw::Libraw::new();
+    let mut manager = Fornax::new(&libraw, &libraw);
+    manager.decode_file(PathBuf::from(
+        "./external/raw-images/images/colorchart-5D2-6000K.dng",
+    ))?;
 
-//     let sizes = processor.image_sizes()?;
-//     println!("{:?}", sizes);
-//     Ok(())
-// }
-fn main() {}
+    let sizes = libraw.image_sizes()?;
+    println!("{:?}", sizes);
+    Ok(())
+}
