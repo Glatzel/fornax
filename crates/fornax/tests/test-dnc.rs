8a462b2199b620ce6df34f599e0ee2ea63c107bd
Glatzel <893016099@qq.com>
2025-04-06 16:23:45 +0800

    fix

--- crates/fornax/tests/test-dnc.rs
+++ crates/fornax/tests/test-dnc.rs
@@ -4,9 +4,9 @@ use assert_cmd::Command;
 #[test]
 fn test_dnc() {
     // This test should only be executed in local machine.
-    if std::env::var("CI").is_ok() {
-        return;
-    }
+    // if std::env::var("CI").is_ok() {
+    //     return;
+    // }
     let root = PathBuf::from(std::env::var("CARGO_WORKSPACE_DIR").unwrap());
     let mut exe = root.clone();
     exe.push("target/llvm-cov-target/debug/examples/dnc.exe");
