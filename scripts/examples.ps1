84f329f6edee7ad71ab8133a96f90f95de3acff4
Glatzel <893016099@qq.com>
2025-04-06 21:50:50 +0800

    fix

--- scripts/examples.ps1
+++ scripts/examples.ps1
@@ -4,6 +4,7 @@ Set-Location $PSScriptRoot/..
 New-Item temp -ItemType Directory -ErrorAction SilentlyContinue
 
 git submodule update --init --recursive
+cargo run --package fornax --example "dnc"
 cargo run --package fornax --example "libraw-imgother"
 cargo run --package fornax --example "libraw-iparams"
 cargo run --package fornax --example "libraw-process-cg"
