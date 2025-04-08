2fd8dfe207876c704505e9c71dbda7452761d852
Glatzel <893016099@qq.com>
2025-04-08 23:07:55 +0800

    fix

--- crates/fornax-py/rattler/ci-fornax-py.ps1
+++ crates/fornax-py/rattler/ci-fornax-py.ps1
@@ -5,11 +5,10 @@ param (
 $ROOT = git rev-parse --show-toplevel
 Set-Location $PSScriptRoot
 
-& "$PSScriptRoot/../scripts/build-cuda.ps1"
-& "$PSScriptRoot/../scripts/maturin-develop.ps1" -config $config
-& "$PSScriptRoot/../scripts/pytest.ps1"
-& "$PSScriptRoot/../scripts/build-python-whl.ps1" -config $config
+& "$ROOT/scripts/maturin-develop.ps1" -config $config
+& "$ROOT/scripts/pytest.ps1"
+& "$ROOT/scripts/build-python-whl.ps1" -config $config
 
 Set-Location $PSScriptRoot
-pixi run rattler-build build
+pixi run -e pydev rattler-build build
 Set-Location $ROOT
