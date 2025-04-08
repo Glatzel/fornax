11930412bca5bc47431ee91c4ad0bf271e9a4152
Glatzel <893016099@qq.com>
2025-04-08 22:52:00 +0800

    fix

--- scripts/build-python-whl.ps1
+++ scripts/build-python-whl.ps1
@@ -4,10 +4,10 @@ param (
 )
 $ROOT = git rev-parse --show-toplevel
 & $PSScriptRoot/setup.ps1
-Set-Location $PSScriptRoot/..
-Remove-Item $ROOT/dist/pyxis*.whl -ErrorAction SilentlyContinue
-Remove-Item ./pyxis/pyxis_py.pyd -ErrorAction SilentlyContinue
-Remove-Item ./pyxis/**__pycache__ -Recurse -ErrorAction SilentlyContinue
+Set-Location $PSScriptRoot/../crates/fornax-py
+Remove-Item ./dist/fornax*.whl -ErrorAction SilentlyContinue
+Remove-Item ./fornax/fornax_py.pyd -ErrorAction SilentlyContinue
+Remove-Item ./**__pycache__ -Recurse -ErrorAction SilentlyContinue
 pixi run maturin build --out ./dist --profile $config
 
 Set-Location $ROOT
