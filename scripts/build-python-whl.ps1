79cdb76b5ef1d5f0949d04a6aacc8673c629aa88
Glatzel <893016099@qq.com>
2025-04-08 22:06:33 +0800

    fix

---
+++ scripts/build-python-whl.ps1
@@ -0,0 +1,13 @@
+param (
+    [ValidateSet("develop", "release")]
+    $config = "develop"
+)
+$ROOT = git rev-parse --show-toplevel
+& $PSScriptRoot/setup.ps1
+Set-Location $PSScriptRoot/..
+Remove-Item $ROOT/dist/pyxis*.whl -ErrorAction SilentlyContinue
+Remove-Item ./pyxis/pyxis_py.pyd -ErrorAction SilentlyContinue
+Remove-Item ./pyxis/**__pycache__ -Recurse -ErrorAction SilentlyContinue
+pixi run maturin build --out ./dist --profile $config
+
+Set-Location $ROOT
