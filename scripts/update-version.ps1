09a1875bc19363cf5af7c13fc2be94519d5392f6
Glatzel <893016099@qq.com>
2025-04-09 19:58:32 +0800

    chore: bump to 0.0.1

---
+++ scripts/update-version.ps1
@@ -0,0 +1,12 @@
+Set-Location $PSScriptRoot/..
+$version= "0.0.1"
+
+# Update the version in Cargo.toml
+$cargoTomlPath = "./Cargo.toml"
+(Get-Content -Path $cargoTomlPath) -replace '^version = .*', "version = `"$version`"" | Set-Content -Path $cargoTomlPath
+Write-Host "Updated Rust version to $version"
+
+# Update python rattler version
+$recipe_path = "./crates/fornax-py/rattler/recipe.yaml"
+(Get-Content -Path $recipe_path) -replace '^  version: .*', "  version: $version" | Set-Content -Path $recipe_path
+Write-Host "Updated ratter version to $version"
