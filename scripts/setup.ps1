56c9bef51ba85310321adb4268ebcf45b5ba6b08
Glatzel <893016099@qq.com>
2025-04-12 07:06:04 +0800

    fix

--- scripts/setup.ps1
+++ scripts/setup.ps1
@@ -1,9 +1,9 @@
 $env:PYTHONPATH = Resolve-Path $PSScriptRoot/../crates/fornax-py
-pixi install -e libraw
+
 if ($IsWindows) {
-    $env:PKG_CONFIG_PATH = Resolve-Path $PSScriptRoot/../Library/lib/pkgconfig
-    # $env:PKG_CONFIG_PATH = Resolve-Path $PSScriptRoot/../vcpkg/installed/x64-windows-static/lib/pkgconfig
+    $env:PKG_CONFIG_PATH = Resolve-Path $PSScriptRoot/../vcpkg/installed/x64-windows-static/lib/pkgconfig
 }
 if ($IsLinux) {
+    pixi install -e libraw
     $env:PKG_CONFIG_PATH = Resolve-Path $PSScriptRoot/../.pixi/envs/libraw/lib/pkgconfig
 }
