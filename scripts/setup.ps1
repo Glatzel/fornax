eaeb95b87bc9fa0a87c6ced5f86401c51b1bc47e
Glatzel <893016099@qq.com>
2025-04-11 23:40:43 +0800

    pixi

--- scripts/setup.ps1
+++ scripts/setup.ps1
@@ -1,3 +1,2 @@
 $env:PYTHONPATH = Resolve-Path $PSScriptRoot/../crates/fornax-py
 pixi install -e libraw
-$env:PKG_CONFIG_PATH = Resolve-Path $PSScriptRoot/../.pixi/envs/libraw/Library/lib/pkgconfig
