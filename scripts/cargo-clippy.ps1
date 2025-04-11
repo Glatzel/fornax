5cdf140a3f4fa6bc8b92d33d27c07a12a5b111fc
Glatzel <893016099@qq.com>
2025-04-11 23:04:35 +0800

    fix

--- scripts/cargo-clippy.ps1
+++ scripts/cargo-clippy.ps1
@@ -3,10 +3,10 @@ Set-Location $PSScriptRoot/..
 & $PSScriptRoot/setup.ps1
 
 if ($env:CI) {
-    pixi run cargo clippy --all-features --workspace --exclude libraw-sys
+    pixi run cargo clippy --all-features --workspace
 }
 else {
-    pixi run cargo clippy --fix --all-features --workspace --exclude libraw-sys
+    pixi run cargo clippy --fix --all-features --workspace
 }
 
 Set-Location $ROOT
