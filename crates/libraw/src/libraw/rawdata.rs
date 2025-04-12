5a5681a59b8a3700d257822571f6c516f8c64d01
Glatzel <893016099@qq.com>
2025-04-12 21:09:20 +0800

    fix

--- crates/libraw/src/libraw/rawdata.rs
+++ crates/libraw/src/libraw/rawdata.rs
@@ -7,7 +7,7 @@ pub struct LibrawRawdata {}
 impl LibrawRawdata {
     pub(crate) fn get_rawdata(
         imgdata: *mut libraw_sys::libraw_data_t,
-        width: usize,
+        width: usize, 
         height: usize,
     ) -> miette::Result<FornaxRawImage> {
         if unsafe { (*imgdata).rawdata.raw_alloc }.is_null() {
@@ -22,6 +22,15 @@ impl LibrawRawdata {
                 .unwrap()
             };
             Ok(FornaxRawImage::Mono16(img))
+        } else if !unsafe { (*imgdata).rawdata.float_image }.is_null() {
+            clerk::debug!("Found mono32 raw image.");
+            let img: image::ImageBuffer<image::Luma<f32>, Vec<f32>> = {
+                ImageBuffer::from_vec(width as u32, height as u32, unsafe {
+                    slice::from_raw_parts((*imgdata).rawdata.float_image, width * height).to_vec()
+                })
+                .unwrap()
+            };
+            Ok(FornaxRawImage::MonoF32(img))
         } else if !unsafe { (*imgdata).rawdata.color3_image }.is_null() {
             clerk::debug!("Found rgb16 raw image.");
             let img: image::ImageBuffer<image::Rgb<u16>, Vec<u16>> =
@@ -46,15 +55,6 @@ impl LibrawRawdata {
                 })
                 .unwrap();
             Ok(FornaxRawImage::Rgba16(img))
-        } else if !unsafe { (*imgdata).rawdata.float_image }.is_null() {
-            clerk::debug!("Found mono32 raw image.");
-            let img: image::ImageBuffer<image::Luma<f32>, Vec<f32>> = {
-                ImageBuffer::from_vec(width as u32, height as u32, unsafe {
-                    slice::from_raw_parts((*imgdata).rawdata.float_image, width * height).to_vec()
-                })
-                .unwrap()
-            };
-            Ok(FornaxRawImage::MonoF32(img))
         } else if !unsafe { (*imgdata).rawdata.float3_image }.is_null() {
             clerk::debug!("Found rgb32 raw image.");
             let img: image::ImageBuffer<image::Rgb<f32>, Vec<f32>> =
