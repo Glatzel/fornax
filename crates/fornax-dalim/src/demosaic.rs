cb0baa0a5ddeb46ca710efc8c512711f5257d632
Glatzel <chenzelin9794@gmail.com>
2025-04-14 08:34:45 +0800

    feat: initial Dalim post processor (#30)
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

---
+++ crates/fornax-dalim/src/demosaic.rs
@@ -0,0 +1,7 @@
+mod linear;
+// pub use linear::DemosaicLinear;
+// pub trait IDemosaic {
+//     fn demosaic(
+//         img: fornax_core::FornaxRawImage,
+//     ) -> image::ImageBuffer<image::Rgb<f32>, Vec<f32>>;
+// }
