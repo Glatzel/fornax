7c25cd874526eefa9608dde56227cdab53439b09
Glatzel <893016099@qq.com>
2025-04-14 21:57:43 +0800

    feat: implement linear demosaicing

--- crates/fornax-core/src/lib.rs
+++ crates/fornax-core/src/lib.rs
@@ -1,11 +1,49 @@
 use std::fmt::Display;
 use std::path::Path;
 #[derive(Debug, PartialEq)]
+pub enum BayerChannel {
+    R,
+    G,
+    B,
+    G2
+}
+
+#[derive(Debug, PartialEq)]
 pub enum BayerPattern {
     RGGB,
     BGGR,
     GRBG,
     GBRG,
+}
+impl BayerPattern {
+    pub fn as_mask(&self) -> &[BayerChannel; 4] {
+        match self {
+            BayerPattern::RGGB => &[
+                BayerChannel::R,
+                BayerChannel::G,
+                BayerChannel::G2,
+                BayerChannel::B,
+            ],
+            BayerPattern::BGGR => &[
+                BayerChannel::B,
+                BayerChannel::G2,
+                BayerChannel::G,
+                BayerChannel::R,
+            ],
+            BayerPattern::GRBG => &[
+                BayerChannel::G,
+                BayerChannel::R,
+                BayerChannel::B,
+                BayerChannel::G2,
+            ],
+            BayerPattern::GBRG => &[
+                BayerChannel::G2,
+                BayerChannel::B,
+                BayerChannel::R,
+                BayerChannel::G,
+            ],
+        }
+    }
 }
 impl Display for BayerPattern {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
@@ -32,7 +70,7 @@ impl BayerImage {
             pattern,
         }
     }
-    pub fn bayer_image(&self) -> &image::ImageBuffer<image::Luma<u16>, Vec<u16>> {
+    pub fn mosaic(&self) -> &image::ImageBuffer<image::Luma<u16>, Vec<u16>> {
         &self.bayer_image
     }
     pub fn pattern(&self) -> &BayerPattern {
--- crates/fornax-dalim/src/demosaic/linear.rs
+++ crates/fornax-dalim/src/demosaic/linear.rs
@@ -1,123 +1,103 @@
-// use rayon::prelude::*;
-// fn get_red_value(
-//     img: &image::ImageBuffer<image::Rgba<u16>, Vec<u16>>,
-//     x: u32,
-//     y: u32,
-//     width: u32,
-//     height: u32,
-// ) -> u16 {
-//     if x > 0 && y > 0 && x < width - 1 && y < height - 1 {
-//         let top_left = &img.get_pixel(x - 1, y - 1);
-//         let top_right = &img.get_pixel(x + 1, y - 1);
-//         let bottom_left = &img.get_pixel(x - 1, y + 1);
-//         let bottom_right = &img.get_pixel(x + 1, y + 1);
-//         (top_left[0] + top_right[0] + bottom_left[0] + bottom_right[0]) / 4
-//     } else {
-//         img.get_pixel(x, y)[0]
-//     }
-// }
-// fn get_green_value(
-//     img: &image::ImageBuffer<image::Rgba<u16>, Vec<u16>>,
-//     x: u32,
-//     y: u32,
-//     width: u32,
-//     height: u32,
-// ) -> u16 {
-//     if x > 0 && y > 0 && x < width - 1 && y < height - 1 {
-//         let left = &img.get_pixel(x - 1, y);
-//         let right = &img.get_pixel(x + 1, y);
-//         let top = &img.get_pixel(x, y - 1);
-//         let buttom = &img.get_pixel(x, y + 1);
-//         (left[1] + right[1] + top[1] + buttom[1] + left[3] + right[3] + top[3] + buttom[3]) / 4
-//     } else {
-//         img.get_pixel(x, y)[0]
-//     }
-// }
-// fn get_blue_value(
-//     img: &image::ImageBuffer<image::Rgba<u16>, Vec<u16>>,
-//     x: u32,
-//     y: u32,
-//     width: u32,
-//     height: u32,
-// ) -> u16 {
-//     if x > 0 && y > 0 && x < width - 1 && y < height - 1 {
-//         let top_left = &img.get_pixel(x - 1, y - 1);
-//         let top_right = &img.get_pixel(x + 1, y - 1);
-//         let bottom_left = &img.get_pixel(x - 1, y + 1);
-//         let bottom_right = &img.get_pixel(x + 1, y + 1);
-//         (top_left[2] + top_right[2] + bottom_left[2] + bottom_right[2]) / 4
-//     } else {
-//         img.get_pixel(x, y)[0]
-//     }
-// }
-// fn get_neighbour_red_blue(
-//     img: &image::ImageBuffer<image::Rgba<u16>, Vec<u16>>,
-//     x: u32,
-//     y: u32,
-//     width: u32,
-//     height: u32,
-// ) -> (u16, u16) {
-//     if x > 0 && y > 0 && x < width - 1 && y < height - 1 {
-//         let left = &img.get_pixel(x - 1, y);
-//         let right = &img.get_pixel(x + 1, y);
-//         let top = &img.get_pixel(x, y - 1);
-//         let buttom = &img.get_pixel(x, y + 1);
-//         let red = (left[0] + right[0] + top[0] + buttom[0]) / 2;
-//         let blue = (left[2] + right[2] + top[2] + buttom[2]) / 2;
-//         (red, blue)
-//     } else {
-//         (img.get_pixel(x, y)[0], img.get_pixel(x, y)[0])
-//     }
-// }
-// pub struct DemosaicLinear();
-// impl DemosaicLinear {
-//     fn demosaic(img: fornax_core::FornaxRawImage) -> image::ImageBuffer<image::Rgb<u16>,
-// Vec<u16>> {         let temp_img = img.clone();
-//         let (width, height) = temp_img.dimensions();
-//         let mut img: image::ImageBuffer<image::Rgb<u16>, Vec<u16>> =
-//             image::ImageBuffer::new(width, height);
-//         img.enumerate_pixels_mut()
-//             .collect::<Vec<(u32, u32, &mut image::Rgb<u16>)>>()
-//             .par_iter_mut()
-//             .for_each(|(x, y, pixel)| {
-//                 //red
-//                 if (*x % 2 == 0) && (*y % 2 == 1) {
-//                     pixel[0] = temp_img.get_pixel(*x, *y)[0];
-//                     pixel[1] = get_green_value(&temp_img, *x, *y, width, height);
-//                     pixel[2] = get_blue_value(&temp_img, *x, *y, width, height);
-//                 }
-//                 //blue
-//                 else if (*x % 2 == 1) && (*y % 2 == 0) {
-//                     pixel[0] = get_red_value(&temp_img, *x, *y, width, height);
-//                     pixel[1] = get_green_value(&temp_img, *x, *y, width, height);
-//                     pixel[2] = temp_img.get_pixel(*x, *y)[2];
-//                 }
-//                 // green
-//                 else {
-//                     (pixel[0], pixel[2]) = get_neighbour_red_blue(&temp_img, *x, *y, width,
-// height);                     let center = temp_img.get_pixel(*x, *y);
-//                     pixel[1] = center[1] + center[3];
-//                 }
-//             });
-//         img
-//     }
-// }
-// #[cfg(test)]
-// mod test {
-//     use std::path::PathBuf;
+use fornax_core::BayerChannel;
+use rayon::prelude::*;
+fn get_diagnal_value(img: &image::ImageBuffer<image::Luma<u16>, Vec<u16>>, x: u32, y: u32) -> u16 {
+    let top_left = img.get_pixel(x - 1, y - 1);
+    let top_right = img.get_pixel(x + 1, y - 1);
+    let bottom_left = img.get_pixel(x - 1, y + 1);
+    let bottom_right = img.get_pixel(x + 1, y + 1);
+    (top_left[0] + top_right[0] + bottom_left[0] + bottom_right[0]) / 4
+}
+fn get_neighbour_value(
+    img: &image::ImageBuffer<image::Luma<u16>, Vec<u16>>,
+    x: u32,
+    y: u32,
+) -> u16 {
+    let left = img.get_pixel(x - 1, y);
+    let right = img.get_pixel(x + 1, y);
+    let top = img.get_pixel(x, y - 1);
+    let buttom = img.get_pixel(x, y + 1);
+    (left[0] + right[0] + top[0] + buttom[0]) / 4
+}
+fn get_left_right_value(
+    img: &image::ImageBuffer<image::Luma<u16>, Vec<u16>>,
+    x: u32,
+    y: u32,
+) -> u16 {
+    let left = img.get_pixel(x - 1, y);
+    let right = img.get_pixel(x + 1, y);
 
-//     use super::*;
-//     #[test]
-//     fn test_linear() {
-//         let root = PathBuf::from(std::env::var("CARGO_WORKSPACE_DIR").unwrap());
-//         let mut img_path = root.clone();
-//         img_path.push("temp/raw_image.tiff");
-//         let img = image::ImageReader::open(img_path)
-//             .unwrap()
-//             .decode()
-//             .unwrap()
-//             .to_rgba16();
-//         let dema = DemosaicLinear::demosaic(img);
-//         image::DynamicImage::from(dema).save("a.tiff").unwrap();
-//     }
-// }
+    (left[0] + right[0]) / 2
+}
+fn get_top_down_value(img: &image::ImageBuffer<image::Luma<u16>, Vec<u16>>, x: u32, y: u32) -> u16 {
+    let top = img.get_pixel(x, y - 1);
+    let buttom = img.get_pixel(x, y + 1);
+    (top[0] + buttom[0]) / 2
+}
+pub struct DemosaicLinear();
+impl super::IDemosaic for DemosaicLinear {
+    fn demosaic(
+        bayer_image: &fornax_core::BayerImage,
+    ) -> image::ImageBuffer<image::Rgb<u16>, Vec<u16>> {
+        let mosaic = bayer_image.mosaic();
+        let pattern = bayer_image.pattern();
+        let (width, height) = mosaic.dimensions();
+        let mut img: image::ImageBuffer<image::Rgb<u16>, Vec<u16>> =
+            image::ImageBuffer::new(width, height);
+        let bayer_mask = pattern.as_mask();
+        img.enumerate_pixels_mut()
+            .collect::<Vec<(u32, u32, &mut image::Rgb<u16>)>>()
+            .par_iter_mut()
+            .for_each(|(x, y, pixel)| {
+                if *x > 0 && *y > 0 && *x < width-1 && *y < height-1 {
+                    // `(*x & 1) + 2 * (*y & 1)` is the index in pattern of the current pixel at image (x,y).
+                    match &bayer_mask[((*x & 1) + 2 * (*y & 1)) as usize] {
+                        BayerChannel::R => {
+                            pixel[0] = mosaic.get_pixel(*x, *y)[0];
+                            pixel[1] = get_neighbour_value(mosaic, *x, *y);
+                            pixel[2] = get_diagnal_value(mosaic, *x, *y);
+                        }
+                        BayerChannel::G => {
+                            pixel[0] = get_left_right_value(mosaic, *x, *y);
+                            pixel[1] = mosaic.get_pixel(*x, *y)[0];
+                            pixel[2] = get_top_down_value(mosaic, *x, *y);
+                        }
+                        BayerChannel::B => {
+                            pixel[0] = get_diagnal_value(mosaic, *x, *y);
+                            pixel[1] = get_neighbour_value(mosaic, *x, *y);
+                            pixel[2] = mosaic.get_pixel(*x, *y)[0];
+                        }
+                        BayerChannel::G2 => {
+                            pixel[0] = get_top_down_value(mosaic, *x, *y);
+                            pixel[1] = mosaic.get_pixel(*x, *y)[0];
+                            pixel[2] = get_left_right_value(mosaic, *x, *y);
+                        }
+                    }
+                }
+            });
+        img
+    }
+}
+#[cfg(test)]
+mod test {
+    use std::path::PathBuf;
+
+    use crate::demosaic::IDemosaic;
+
+    use super::*;
+    #[test]
+    fn test_linear() {
+        let root = PathBuf::from(std::env::var("CARGO_WORKSPACE_DIR").unwrap());
+        let mut img_path = root.clone();
+        img_path.push("temp/bayerimga.tiff");
+        let bayer = image::ImageReader::open(img_path)
+            .unwrap()
+            .decode()
+            .unwrap()
+            .to_luma16();
+        let img = DemosaicLinear::demosaic(&fornax_core::BayerImage::new(
+            bayer,
+            fornax_core::BayerPattern::GBRG,
+        ));
+        image::DynamicImage::from(img).save("a.tiff").unwrap();
+    }
+}
