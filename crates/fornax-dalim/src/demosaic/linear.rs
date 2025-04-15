4775a2555cb850481014c1d31ed0460733cf4eef
Glatzel <893016099@qq.com>
2025-04-15 12:33:25 +0800

    fix

--- crates/fornax-dalim/src/demosaic/linear.rs
+++ crates/fornax-dalim/src/demosaic/linear.rs
@@ -1,13 +1,6 @@
 use fornax_core::{BayerChannel, BayerImage, FornaxBayerImage};
-use image::{GenericImage, ImageBuffer};
-use itertools::Itertools;
+use image::ImageBuffer;
 
-struct MyIterator {
-    x_start: u32,
-    x_end: u32,
-    y_start: u32,
-    y_end: u32,
-}
 use rayon::prelude::*;
 fn get_diagnal_value(img: &FornaxBayerImage, x: u32, y: u32) -> u16 {
     let top_left = img.get_pixel(x - 1, y - 1);
@@ -37,6 +30,110 @@ fn get_top_down_value(img: &FornaxBayerImage, x: u32, y: u32) -> u16 {
     let buttom = img.get_pixel(x, y + 1);
     (top[0] + buttom[0]) / 2
 }
+fn get_diagnal_value_check(img: &FornaxBayerImage, x: u32, y: u32) -> u16 {
+    let mut count = 0;
+    let top_left = match img.get_pixel_checked(x - 1, y - 1) {
+        Some(p) => {
+            count += 1;
+            p[0]
+        }
+        None => 0,
+    };
+    let top_right = match img.get_pixel_checked(x + 1, y - 1) {
+        Some(p) => {
+            count += 1;
+            p[0]
+        }
+        None => 0,
+    };
+    let bottom_left = match img.get_pixel_checked(x - 1, y + 1) {
+        Some(p) => {
+            count += 1;
+            p[0]
+        }
+        None => 0,
+    };
+    let bottom_right = match img.get_pixel_checked(x + 1, y + 1) {
+        Some(p) => {
+            count += 1;
+            p[0]
+        }
+        None => 0,
+    };
+    (top_left + top_right + bottom_left + bottom_right) / count
+}
+
+fn get_neighbour_value_check(img: &FornaxBayerImage, x: u32, y: u32) -> u16 {
+    let mut count = 0;
+    let left = match img.get_pixel_checked(x - 1, y) {
+        Some(p) => {
+            count += 1;
+            p[0]
+        }
+        None => 0,
+    };
+    let right = match img.get_pixel_checked(x + 1, y) {
+        Some(p) => {
+            count += 1;
+            p[0]
+        }
+        None => 0,
+    };
+    let top = match img.get_pixel_checked(x, y - 1) {
+        Some(p) => {
+            count += 1;
+            p[0]
+        }
+        None => 0,
+    };
+    let buttom = match img.get_pixel_checked(x, y + 1) {
+        Some(p) => {
+            count += 1;
+            p[0]
+        }
+        None => 0,
+    };
+    (left + right + top + buttom) / count
+}
+
+fn get_left_right_value_check(img: &FornaxBayerImage, x: u32, y: u32) -> u16 {
+    let mut count = 0;
+    let left = match img.get_pixel_checked(x - 1, y) {
+        Some(p) => {
+            count += 1;
+            p[0]
+        }
+        None => 0,
+    };
+    let right = match img.get_pixel_checked(x + 1, y) {
+        Some(p) => {
+            count += 1;
+            p[0]
+        }
+        None => 0,
+    };
+
+    (left + right) / count
+}
+
+fn get_top_down_value_check(img: &FornaxBayerImage, x: u32, y: u32) -> u16 {
+    let mut count = 0;
+    let top = match img.get_pixel_checked(x, y - 1) {
+        Some(p) => {
+            count += 1;
+            p[0]
+        }
+        None => 0,
+    };
+    let buttom = match img.get_pixel_checked(x, y + 1) {
+        Some(p) => {
+            count += 1;
+            p[0]
+        }
+        None => 0,
+    };
+    (top + buttom) / count
+}
 pub struct DemosaicLinear();
 impl super::IDemosaic for DemosaicLinear {
     fn demosaic(bayer_image: &BayerImage) -> ImageBuffer<image::Rgb<u16>, Vec<u16>> {
@@ -45,34 +142,58 @@ impl super::IDemosaic for DemosaicLinear {
         let (width, height) = mosaic.dimensions();
         let mut img: ImageBuffer<image::Rgb<u16>, Vec<u16>> = ImageBuffer::new(width, height);
         let bayer_mask = pattern.as_mask();
+        clerk::info!("Start demosaicing.");
         img.par_enumerate_pixels_mut().for_each(|(x, y, pixel)| {
-            if x > 0 && y > 0 && x < width - 1 && y < height - 1 {
-                // `(*x & 1) + 2 * (*y & 1)` is the of the current pixel at image (x,y) index in
-                // bayer pattern.
-                match &bayer_mask[((x & 1) + 2 * (y & 1)) as usize] {
-                    BayerChannel::R => {
-                        pixel[0] = mosaic.get_pixel(x, y)[0];
-                        pixel[1] = get_neighbour_value(mosaic, x, y);
-                        pixel[2] = get_diagnal_value(mosaic, x, y);
-                    }
-                    BayerChannel::G => {
-                        pixel[0] = get_left_right_value(mosaic, x, y);
-                        pixel[1] = mosaic.get_pixel(x, y)[0];
-                        pixel[2] = get_top_down_value(mosaic, x, y);
-                    }
-                    BayerChannel::B => {
-                        pixel[0] = get_diagnal_value(mosaic, x, y);
-                        pixel[1] = get_neighbour_value(mosaic, x, y);
-                        pixel[2] = mosaic.get_pixel(x, y)[0];
-                    }
-                    BayerChannel::G2 => {
-                        pixel[0] = get_top_down_value(mosaic, x, y);
-                        pixel[1] = mosaic.get_pixel(x, y)[0];
-                        pixel[2] = get_left_right_value(mosaic, x, y);
-                    }
+            // `(*x & 1) + 2 * (*y & 1)` is the of the current pixel at image (x,y) index in
+            // bayer pattern.
+
+            match (
+                &bayer_mask[((x & 1) + 2 * (y & 1)) as usize],
+                x > 0 && y > 0 && x < width - 1 && y < height - 1,
+            ) {
+                (BayerChannel::R, true) => {
+                    pixel[0] = mosaic.get_pixel(x, y)[0];
+                    pixel[1] = get_neighbour_value(mosaic, x, y);
+                    pixel[2] = get_diagnal_value(mosaic, x, y);
+                }
+                (BayerChannel::G, true) => {
+                    pixel[0] = get_left_right_value(mosaic, x, y);
+                    pixel[1] = mosaic.get_pixel(x, y)[0];
+                    pixel[2] = get_top_down_value(mosaic, x, y);
+                }
+                (BayerChannel::B, true) => {
+                    pixel[0] = get_diagnal_value(mosaic, x, y);
+                    pixel[1] = get_neighbour_value(mosaic, x, y);
+                    pixel[2] = mosaic.get_pixel(x, y)[0];
+                }
+                (BayerChannel::G2, true) => {
+                    pixel[0] = get_top_down_value(mosaic, x, y);
+                    pixel[1] = mosaic.get_pixel(x, y)[0];
+                    pixel[2] = get_left_right_value(mosaic, x, y);
+                }
+                (BayerChannel::R, false) => {
+                    pixel[0] = mosaic.get_pixel(x, y)[0];
+                    pixel[1] = get_neighbour_value_check(mosaic, x, y);
+                    pixel[2] = get_diagnal_value_check(mosaic, x, y);
+                }
+                (BayerChannel::G, false) => {
+                    pixel[0] = get_left_right_value_check(mosaic, x, y);
+                    pixel[1] = mosaic.get_pixel(x, y)[0];
+                    pixel[2] = get_top_down_value_check(mosaic, x, y);
+                }
+                (BayerChannel::B, false) => {
+                    pixel[0] = get_diagnal_value_check(mosaic, x, y);
+                    pixel[1] = get_neighbour_value_check(mosaic, x, y);
+                    pixel[2] = mosaic.get_pixel(x, y)[0];
+                }
+                (BayerChannel::G2, false) => {
+                    pixel[0] = get_top_down_value_check(mosaic, x, y);
+                    pixel[1] = mosaic.get_pixel(x, y)[0];
+                    pixel[2] = get_left_right_value_check(mosaic, x, y);
                 }
             }
         });
+        clerk::info!("End demosaicing.");
         img
     }
 }
@@ -80,10 +201,16 @@ impl super::IDemosaic for DemosaicLinear {
 mod test {
     use std::path::PathBuf;
 
+    use tracing::level_filters::LevelFilter;
+    use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+
     use super::*;
     use crate::demosaic::IDemosaic;
     #[test]
     fn test_linear() {
+        tracing_subscriber::registry()
+            .with(clerk::terminal_layer(LevelFilter::DEBUG, true))
+            .init();
         let root = PathBuf::from(std::env::var("CARGO_WORKSPACE_DIR").unwrap());
         let mut img_path = root.clone();
         img_path.push("temp/bayerimga.tiff");
@@ -96,6 +223,6 @@ mod test {
             bayer,
             fornax_core::BayerPattern::GBRG,
         ));
-        // image::DynamicImage::from(img).save("a.tiff").unwrap();
+        image::DynamicImage::from(img).save("a.tiff").unwrap();
     }
 }
