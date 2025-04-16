dc449b64cefde1d49e4364dcc3c325282de818f0
Glatzel <chenzelin9794@gmail.com>
2025-04-17 00:20:17 +0800

    ci: Update build.rs (#42)

--- crates/libraw-sys/build.rs
+++ crates/libraw-sys/build.rs
@@ -47,15 +47,11 @@ fn main() {
             .unwrap();
 
         bindings
-            .write_to_file("src/bindings.rs")
-            .expect("Couldn't write bindings!");
-        clerk::info!("Build bingings to: src/bindings.rs");
-        bindings
-            .write_to_file(PathBuf::from(std::env::var("OUT_DIR").unwrap()).join("src/bindings.rs"))
+            .write_to_file(PathBuf::from(std::env::var("OUT_DIR").unwrap()).join("bindings.rs"))
             .expect("Couldn't write bindings!");
         clerk::info!(
             "Build bingings to: {:?}",
-            PathBuf::from(std::env::var("OUT_DIR").unwrap()).join("src/bindings.rs")
+            PathBuf::from(std::env::var("OUT_DIR").unwrap()).join("bindings.rs")
         );
     }
 }
