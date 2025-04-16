b873d793b432d3e21ae801d259b5b904f5483b42
Glatzel <893016099@qq.com>
2025-04-16 16:53:00 +0800

    fix

--- crates/fornax/examples/dnc.rs
+++ crates/fornax/examples/dnc.rs
@@ -24,19 +24,20 @@ fn default_path() -> miette::Result<()> {
         .decode_file(&dng_file)?
         .post_process()?
         .to_dynamic_image();
-    img.save("temp/dng-converter.tiff").into_diagnostic()?;
-    clerk::info!("save img to: temp/dng-converter.tiff");
+    img.save(utils::output_dir().join("dnc-default-path.tiff"))
+        .into_diagnostic()?;
+    clerk::info!("Done saving raw image.");
     Ok(())
 }
 
 fn custom_path() -> miette::Result<()> {
     let dnc = dnc::Dnc::new(dnc::DncParams {
-        directory: Some(PathBuf::from("./temp")),
+        directory: Some(utils::output_dir()),
         filename: Some("dng-converter.dng".to_string()),
         overwrite: true,
         ..Default::default()
     });
-    let dng_file = dnc.convert(&utils::raw_file())?;
+    let dng_file = dnc.convert(&utils::output_dir().join("dng-converter.dng"))?;
     let libraw = libraw::Libraw::default();
     let mut manager = Fornax::new(&libraw, &libraw);
     let img = manager
@@ -44,8 +45,8 @@ fn custom_path() -> miette::Result<()> {
         .post_process()?
         .to_dynamic_image();
 
-    img.save(utils::output_dir().join("dnc.tiff"))
+    img.save(utils::output_dir().join("dnc-custom-path.tiff"))
         .into_diagnostic()?;
-    clerk::info!("save img to: dnc.tiff");
+    clerk::info!("Done saving raw image.");
     Ok(())
 }
