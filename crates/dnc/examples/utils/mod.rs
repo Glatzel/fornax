17eaf4bc9d18bb9f7ac5327690a9c0f2ceea1939
Glatzel <893016099@qq.com>
2025-04-19 07:05:41 +0800

    fix

---
+++ crates/dnc/examples/utils/mod.rs
@@ -0,0 +1,25 @@
+use std::path::PathBuf;
+
+use tracing::level_filters::LevelFilter;
+use tracing_subscriber::layer::SubscriberExt;
+use tracing_subscriber::util::SubscriberInitExt;
+
+pub fn example_setup() {
+    tracing_subscriber::registry()
+        .with(clerk::terminal_layer(LevelFilter::DEBUG, true))
+        .init();
+    let outdir = output_dir();
+    std::fs::create_dir_all(outdir).expect("output dir already exists.");
+}
+
+pub fn root_dir() -> PathBuf {
+    PathBuf::from(std::env::var("CARGO_WORKSPACE_DIR").unwrap())
+}
+pub fn output_dir() -> PathBuf {
+    let outdir: PathBuf = root_dir().join("temp/fornax/example");
+    outdir
+}
+
+pub fn raw_file() -> PathBuf {
+    root_dir().join("external/raw-images/images/colorchart-eos-7d.cr2")
+}
