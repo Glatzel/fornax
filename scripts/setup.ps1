df8c12ee0f17dddedd88a14790df6468da2dc560
Glatzel <893016099@qq.com>
2025-04-18 11:36:03 +0800

    fix

--- scripts/setup.ps1
+++ scripts/setup.ps1
@@ -1,9 +1,9 @@
 $env:PYTHONPATH = Resolve-Path $PSScriptRoot/../crates/fornax-py
 pixi install
 if ($IsWindows) {
-    $env:PKG_CONFIG_PATH = Resolve-Path $PSScriptRoot/../.pixi/libraw/x64-windows-static/lib/pkgconfig
+    $env:PKG_CONFIG_PATH = Resolve-Path $PSScriptRoot/../.pixi/envs/default/libraw/x64-windows-static/lib/pkgconfig
 }
 
 if ($IsLinux) {
-    $env:PKG_CONFIG_PATH = Resolve-Path $PSScriptRoot/../.pixi/libraw/x64-linux-release/lib/pkgconfig
+    $env:PKG_CONFIG_PATH = Resolve-Path $PSScriptRoot/../.pixi/envs/default/libraw/x64-linux-release/lib/pkgconfig
 }
