c98b2272ea2d61c03fd29575ea9b025304d313cb
Glatzel <893016099@qq.com>
2025-04-29 17:33:14 +0800

    fix

--- crates/libraw-sys/src/lib.rs
+++ crates/libraw-sys/src/lib.rs
@@ -1,9 +1,10 @@
 #![allow(
-    non_camel_case_types,
-    non_upper_case_globals,
     clippy::approx_constant,
     clippy::redundant_static_lifetimes,
-    non_snake_case
+    improper_ctypes
+    non_camel_case_types,
+    non_snake_case,
+    non_upper_case_globals,
 )]
 #![no_std]
 
