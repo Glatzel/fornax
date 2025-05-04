5ce8f795ef91e121f6d059db9f80aa4a275593b4
Glatzel <893016099@qq.com>
2025-05-04 17:01:25 +0800

    fix

--- crates/libraw-sys/src/lib.rs
+++ crates/libraw-sys/src/lib.rs
@@ -1,4 +1,9 @@
-#![allow(non_camel_case_types, non_upper_case_globals, non_snake_case)]
+#![allow(
+    improper_ctypes,
+    non_camel_case_types,
+    non_snake_case,
+    non_upper_case_globals
+)]
 #[cfg(not(feature = "bindgen"))]
 include!("bindings.rs");
 #[cfg(feature = "bindgen")]
