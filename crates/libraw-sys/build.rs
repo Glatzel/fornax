94939ae7aab48e5a46dddf1a6dc78aa843378d2d
Glatzel <chenzelin9794@gmail.com>
2025-05-04 17:43:42 +0800

    build: update libraw bindings (#81)

--- crates/libraw-sys/build.rs
+++ crates/libraw-sys/build.rs
@@ -1,19 +1,13 @@
+#[allow(unused_imports)]
 use std::collections::HashSet;
-use std::fmt::Debug;
+#[allow(unused_imports)]
 use std::path::PathBuf;
 
-use tracing::level_filters::LevelFilter;
-use tracing_subscriber::layer::SubscriberExt;
-use tracing_subscriber::util::SubscriberInitExt;
 fn main() {
-    tracing_subscriber::registry()
-        .with(clerk::terminal_layer(LevelFilter::DEBUG, true))
-        .init();
-
     // check LIBCLANG_PATH
     #[cfg(target_os = "windows")]
     match std::env::var("LIBCLANG_PATH") {
-        Ok(path) => tracing::info!("Found `LIBCLANG_PATH`: {path}"),
+        Ok(path) => println!("Found `LIBCLANG_PATH`: {path}"),
         Err(_) => {
             let path = "C:/Program Files/LLVM/bin";
 
@@ -21,28 +15,18 @@ fn main() {
                 unsafe {
                     std::env::set_var("LIBCLANG_PATH", path);
                 }
-                tracing::info!("Set `LIBCLANG_PATH` to: {path}")
+                println!("Set `LIBCLANG_PATH` to: {path}")
             } else {
-                tracing::error!("`LIBCLANG_PATH` not found.");
                 panic!("`LIBCLANG_PATH` not found.");
             }
         }
     };
 
     // Link
-    #[cfg(target_os = "windows")]
     let _pk_libraw = link_lib("libraw_r", "raw_r");
-    #[cfg(target_os = "linux")]
-    let _pk_libraw = link_lib("libraw_r", "libraw_r");
-
     // generate bindings
-    #[cfg(feature = "bindgen")]
+    #[cfg(any(feature = "bindgen", feature = "update"))]
     {
-        let header = &_pk_libraw.include_paths[0]
-            .join("libraw/libraw.h")
-            .to_string_lossy()
-            .to_string();
-
         let ignored_macros = IgnoreMacros(
             vec![
                 "FP_INFINITE".into(),
@@ -55,15 +39,16 @@ fn main() {
             .collect(),
         );
 
+        let header = &_pk_libraw.include_paths[0]
+            .join("libraw/libraw.h")
+            .to_string_lossy()
+            .to_string();
+
         let bindings = bindgen::Builder::default()
             .header(header)
-            .use_core()
-            .derive_eq(true)
-            .ctypes_prefix("libc")
-            .parse_callbacks(Box::new(bindgen::CargoCallbacks::new()))
+            // .size_t_is_usize(true)
+            // .blocklist_type("max_align_t")
             .parse_callbacks(Box::new(ignored_macros))
-            .generate_comments(true)
-            .formatter(bindgen::Formatter::Rustfmt)
             .generate()
             .unwrap();
 
@@ -74,7 +59,7 @@ fn main() {
         bindings
             .write_to_file("./src/bindings.rs")
             .expect("Couldn't write bindings!");
-        clerk::info!(
+        println!(
             "Build bingings to: {:?}",
             PathBuf::from(std::env::var("OUT_DIR").unwrap()).join("bindings.rs")
         );
@@ -84,15 +69,16 @@ fn link_lib(name: &str, lib: &str) -> pkg_config::Library {
     match pkg_config::Config::new().probe(name) {
         Ok(pklib) => {
             println!("cargo:rustc-link-lib=static={}", lib);
-            clerk::info!("Link to `{}`", lib);
+            println!("Link to `{}`", lib);
             pklib
         }
         Err(e) => panic!("cargo:warning=Pkg-config error: {:?}", e),
     }
 }
+#[cfg(feature = "bindgen")]
 #[derive(Debug)]
 struct IgnoreMacros(HashSet<String>);
-
+#[cfg(feature = "bindgen")]
 impl bindgen::callbacks::ParseCallbacks for IgnoreMacros {
     fn will_parse_macro(&self, name: &str) -> bindgen::callbacks::MacroParsingBehavior {
         if self.0.contains(name) {
--- crates/libraw-sys/src/lib.rs
+++ crates/libraw-sys/src/lib.rs
@@ -1,16 +1,13 @@
 #![allow(
     clippy::approx_constant,
-    clippy::redundant_static_lifetimes,
     improper_ctypes,
     non_camel_case_types,
     non_snake_case,
     non_upper_case_globals
 )]
-#![no_std]
-
+#[cfg(all(not(feature = "bindgen"), target_os = "windows"))]
+include!("bindings-win.rs");
+#[cfg(all(not(feature = "bindgen"), target_os = "linux"))]
+include!("bindings-linux.rs");
 #[cfg(feature = "bindgen")]
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
-#[cfg(not(feature = "bindgen"))]
-mod bindings;
-#[cfg(not(feature = "bindgen"))]
-pub use bindings::*;
