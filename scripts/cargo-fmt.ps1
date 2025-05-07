4e9121195758b6e4f0a3d2de2bf11d5b44978618
Glatzel <chenzelin9794@gmail.com>
2025-05-08 04:38:41 +0800

    Update cargo-fmt.ps1

--- scripts/cargo-fmt.ps1
+++ scripts/cargo-fmt.ps1
@@ -3,11 +3,5 @@ $PSNativeCommandUseErrorActionPreference = $true
 $ROOT = git rev-parse --show-toplevel
 Set-Location $PSScriptRoot/..
 & $PSScriptRoot/setup.ps1
-if ($env:CI) {
-    pixi run cargo +nightly fmt --all -- --check
-}
-else {
-    pixi run cargo +nightly fmt --all
-}
-
+pixi run cargo +nightly fmt --all --check
 Set-Location $ROOT
