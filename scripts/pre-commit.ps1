eb7a517772aa8ba8abf7dc487cf247f6a794a96a
Glatzel <893016099@qq.com>
2025-05-08 09:48:15 +0800

    fix

--- scripts/pre-commit.ps1
+++ scripts/pre-commit.ps1
@@ -2,5 +2,6 @@ Set-Location $PSScriptRoot/..
 New-Item ./pre-commit -ItemType Directory -ErrorAction SilentlyContinue
 curl -L -o .pre-commit-config.yaml  https://raw.githubusercontent.com/Glatzel/template/main//.pre-commit-config.yaml
 curl -L -o ./pre-commit/pixi.toml  https://raw.githubusercontent.com/Glatzel/template/main//pre-commit/pixi.toml
+pixi update --manifest-path ./pre-commit/pixi.toml
 pixi install --manifest-path ./pre-commit/pixi.toml
 pixi run --manifest-path ./pre-commit/pixi.toml pre-commit-run --color=always --show-diff-on-failure
