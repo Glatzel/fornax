21931817f1578d4ff0a87740cc9d441104181d10
Glatzel <chenzelin9794@gmail.com>
2025-05-08 11:30:58 +0800

    chore: remove some scripts (#94)
    Co-authored-by: pre-commit-ci-lite[bot] <117423508+pre-commit-ci-lite[bot]@users.noreply.github.com>

--- scripts/numpydoc.ps1
+++
@@ -1,6 +0,0 @@
-$ROOT = git rev-parse --show-toplevel
-& $PSScriptRoot/setup.ps1
-Set-Location $PSScriptRoot/../crates/fornax-py
-$files = Get-ChildItem -Path "./fornax/*.py" -Recurse
-pixi run -e pydev numpydoc lint $files
-Set-Location $ROOT
