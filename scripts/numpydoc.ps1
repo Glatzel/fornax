f83df253bae805c80c16a9cca4883ac8938bb8cb
Glatzel <chenzelin9794@gmail.com>
2025-05-08 09:08:20 +0800

    ci: Update ci.yml (#90)
    Co-authored-by: pre-commit-ci-lite[bot] <117423508+pre-commit-ci-lite[bot]@users.noreply.github.com>

--- scripts/numpydoc.ps1
+++ scripts/numpydoc.ps1
@@ -2,5 +2,5 @@ $ROOT = git rev-parse --show-toplevel
 & $PSScriptRoot/setup.ps1
 Set-Location $PSScriptRoot/../crates/fornax-py
 $files = Get-ChildItem -Path "./fornax/*.py" -Recurse
-pixi run numpydoc lint $files
+pixi run -e pydev numpydoc lint $files
 Set-Location $ROOT
