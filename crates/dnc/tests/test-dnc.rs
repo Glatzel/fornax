9d1ed496cf1762039866a35260c1393902d1d733
Glatzel <893016099@qq.com>
2025-05-14 16:57:08 +0800

    fix

--- crates/dnc/tests/test-dnc.rs
+++ crates/dnc/tests/test-dnc.rs
@@ -3,8 +3,7 @@ use std::path::PathBuf;
 use assert_cmd::Command;
 #[test]
 fn test_dnc() {
-    let root = PathBuf::from(std::env::var("CARGO_WORKSPACE_DIR").unwrap());
-    let mut exe = root.clone();
-    exe.push("target/llvm-cov-target/debug/examples/dnc.exe");
+    let root = fornax_devtool::root_dir();
+    let exe = fornax_devtool::get_example_exe("dnc");
     Command::new(exe).current_dir(root).assert().success();
 }
---
+++ crates/fornax/tests/test_examples.rs
@@ -0,0 +1,17 @@
+use std::path::PathBuf;
+
+use assert_cmd::Command;
+#[test]
+fn test_libraw_dalim() {
+    let root = fornax_devtool::root_dir();
+    let exe = fornax_devtool::get_example_exe("libraw_dalim");
+    Command::new(exe).current_dir(root).assert().success();
+}
+
+#[test]
+fn test_libraw_libraw() {
+    let root = fornax_devtool::root_dir();
+    let exe = fornax_devtool::get_example_exe("libraw_libraw");
+    Command::new(exe).current_dir(root).assert().success();
+}
+}
