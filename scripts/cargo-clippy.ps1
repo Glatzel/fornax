bb7e4e5b3994282b6c41ae608ac1cf702a485fa6
Glatzel <chenzelin9794@gmail.com>
2025-05-30 12:47:13 +0800

    chore: Update cargo-clippy.ps1 (#123)

--- scripts/cargo-clippy.ps1
+++ scripts/cargo-clippy.ps1
@@ -1,4 +1,7 @@
+$ErrorActionPreference = "Stop"
+$PSNativeCommandUseErrorActionPreference = $true
 $ROOT = git rev-parse --show-toplevel
 Set-Location $PSScriptRoot/..
-pixi run cargo +stable clippy --fix --all -- -D warnings
+cargo +stable clippy --fix --all
+cargo +stable clippy -- -Dwarnings
 Set-Location $ROOT
