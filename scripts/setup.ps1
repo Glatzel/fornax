6a040d8aa25f1db20d53d78f6822cbb5c11a693f
Glatzel <893016099@qq.com>
2025-07-23 07:56:18 +0800

    f

--- scripts/setup.ps1
+++ scripts/setup.ps1
@@ -6,4 +6,7 @@ if ($IsWindows) {
     $env:Path = "$bin" + ";" + "$env:Path"
     $env:PKG_CONFIG_PATH = Resolve-Path $PSScriptRoot/../.pixi/envs/default/libraw/x64-windows-static/lib/pkgconfig
 }
+if ($IsLinux) {
+    $env:PKG_CONFIG_PATH = Resolve-Path $PSScriptRoot/../.pixi/envs/default/libraw/x64-linux-release/lib/pkgconfig
+}
 Set-Location $ROOT
