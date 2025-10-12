5399c8d087084b7b4afd23e7e5cce014e16cd842
Glatzel <893016099@qq.com>
2025-10-12 22:56:58 +0800

    f

--- crates/libraw-sys/build.rs
+++ crates/libraw-sys/build.rs
@@ -8,7 +8,7 @@ fn main() {
     #[cfg(target_os = "linux")]
     let _pk_libraw = link_lib("libraw_r", "raw_r");
     #[cfg(target_os = "linux")]
-    println!("cargo:rustc-link-lib=static=m");
+    println!("cargo:rustc-link-lib=m");
 
     // generate bindings
     if env::var("UPDATE").unwrap_or("false".to_string()) == "true"
