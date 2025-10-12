484e8a0cd7e4b29c04032c3c427518e672f55bca
Glatzel <chenzelin9794@gmail.com>
2025-10-13 00:35:50 +0800

    Update build.rs

--- crates/libraw-sys/build.rs
+++ crates/libraw-sys/build.rs
@@ -8,7 +8,7 @@ fn main() {
     {
         println!("cargo:rustc-link-lib=m");
         println!("cargo:rustc-link-lib=stdc++");
-        println!("cargo:rustc-link-lib=dylib=gomp");
+        println!("cargo:rustc-link-lib=static=gomp");
     }
     // generate bindings
     if env::var("UPDATE").unwrap_or("false".to_string()) == "true"
