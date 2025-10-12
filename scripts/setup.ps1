0c5dc255e842cd693c2cbf8b2339ea58435d2638
Glatzel <893016099@qq.com>
2025-10-12 16:07:35 +0800

    f

--- scripts/setup.ps1
+++ scripts/setup.ps1
@@ -1,6 +1,8 @@
 $ROOT = git rev-parse --show-toplevel
 Set-Location $PSScriptRoot/..
 pixi install
+git submodule update --init --recursive
+$env:CONDA_PREFIX=resolve-path $PSScriptRoot/../.pixi/envs/default
 if ($IsWindows) {
     $bin = Resolve-Path $PSScriptRoot/../.pixi/envs/default/Library/bin
     $env:Path = "$bin" + ";" + "$env:Path"
