db6d5a2526c36908710792fd61a62bec480c46f6
Glatzel <893016099@qq.com>
2025-10-13 09:17:31 +0800

    f

--- scripts/install-dnc.ps1
+++ scripts/install-dnc.ps1
@@ -19,26 +19,5 @@ if ($IsWindows) {
     Write-Output "::endgroup::"
 }
 if ($IsMacOS) {
-    if (-not (Test-Path "$ROOT/temp/dnc$version.dmg")) {
-        Write-Output "::group::download dnc $version"
-        aria2c -c -x16 -s16 `
-            -d "$ROOT/temp" `
-            "https://download.adobe.com/pub/adobe/dng/mac/DNGConverter_$version.dmg" `
-            -o "dnc$version.dmg"
-        Write-Output "::endgroup::"
-    }
-    Write-Output "::group::install dnc $version"
-    # Mount the DMG
-    $mount = hdiutil attach "$ROOT/temp/dnc$version.dmg" -nobrowse -quiet | Select-String "/Volumes/" | ForEach-Object { $_.ToString().Trim() }
-    $appPath = Get-ChildItem "$mount" -Filter "*.app" | Select-Object -First 1
-
-    # Copy to Applications
-    if ($appPath) {
-        Copy-Item -r $appPath "/Applications/"
-    }
-
-    # Unmount
-    hdiutil detach "$mount" -quiet
-    Write-Output "dnc installed"
-    Write-Output "::endgroup::"
+   brew install --cask adobe-dng-converter
 }
