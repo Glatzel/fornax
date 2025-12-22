429818ece482a3757017394f2f9acae85e589109
Glatzel <chenzelin9794@gmail.com>
2025-12-22 10:21:26 +0800

    Update __init__.pyi

--- crates/fornax-py/fornax/__init__.pyi
+++ crates/fornax-py/fornax/__init__.pyi
@@ -8,7 +8,8 @@ from ._tracing import LogLevel, init_tracing
 if sys.platform in ("win32", "darwin"):  # noqa: PYI007
     from . import dnc  # noqa: F401
 
-__all__ = ["Fornax", "FornaxOutputBits", "LogLevel", "decoder", "init_tracing", "post_processor"]
+__all__ = ["Fornax", "FornaxOutputBits", "LogLevel", "decoder", "init_tracing", "post_processor",]
 
 if sys.platform in ("win32", "darwin"):  # noqa: PYI007
     __all__.append("dnc")  # noqa: PYI056
+
