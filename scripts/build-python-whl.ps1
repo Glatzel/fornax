88a4a4a43b8ee36dc1d1b1a285b242c3a09f829c
Glatzel <chenzelin9794@gmail.com>
2025-12-28 20:45:37 +0800

    f

--- scripts/build-python-whl.ps1
+++ scripts/build-python-whl.ps1
@@ -1,8 +1,5 @@
-$ROOT = git rev-parse --show-toplevel
 Set-Location $PSScriptRoot/../crates/fornax-py
 Remove-Item ./dist/fornax*.whl -ErrorAction SilentlyContinue
 Remove-Item ./fornax/fornax_py.pyd -ErrorAction SilentlyContinue
 Remove-Item ./**__pycache__ -Recurse -ErrorAction SilentlyContinue
 pixi run maturin build --out ./dist --profile release
-
-Set-Location $ROOT
