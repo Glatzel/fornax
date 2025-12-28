515f672d074dbd957b174a875353d5b1d307449e
Glatzel <chenzelin9794@gmail.com>
2025-12-28 20:44:49 +0800

    feat: build release package in demon forge

--- scripts/build-python-whl.ps1
+++ scripts/build-python-whl.ps1
@@ -1,12 +1,8 @@
-param (
-    [ValidateSet("develop", "release")]
-    $config = "develop"
-)
 $ROOT = git rev-parse --show-toplevel
 Set-Location $PSScriptRoot/../crates/fornax-py
 Remove-Item ./dist/fornax*.whl -ErrorAction SilentlyContinue
 Remove-Item ./fornax/fornax_py.pyd -ErrorAction SilentlyContinue
 Remove-Item ./**__pycache__ -Recurse -ErrorAction SilentlyContinue
-pixi run maturin build --out ./dist --profile $config
+pixi run maturin build --out ./dist --profile release
 
 Set-Location $ROOT
