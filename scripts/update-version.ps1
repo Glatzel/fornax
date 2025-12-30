614aa2f670533987052dc7b2ebefb2d134d24896
Glatzel <chenzelin9794@gmail.com>
2025-12-30 20:23:41 +0800

    build: move apt command from script to ci (#294)
    Co-authored-by: pre-commit-ci-lite[bot] <117423508+pre-commit-ci-lite[bot]@users.noreply.github.com>

--- scripts/update-version.ps1
+++ scripts/update-version.ps1
@@ -1,12 +1,7 @@
 Set-Location $PSScriptRoot/..
-$version= "0.0.11"
+$version= "0.0.12"
 
 # Update the version in Cargo.toml
 $cargoTomlPath = "./Cargo.toml"
 (Get-Content -Path $cargoTomlPath) -replace '^version = .*', "version = `"$version`"" | Set-Content -Path $cargoTomlPath
 Write-Host "Updated Rust version to $version"
-
-# Update python rattler version
-$recipe_path = "./crates/fornax-py/rattler/recipe.yaml"
-(Get-Content -Path $recipe_path) -replace '^  version: .*', "  version: $version" | Set-Content -Path $recipe_path
-Write-Host "Updated ratter version to $version"
