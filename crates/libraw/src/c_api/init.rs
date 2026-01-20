d53a9696c83737aaf480ba497925b30bbaa792d3
Glatzel <chenzelin9794@gmail.com>
2026-01-20 13:37:27 +0800

    f

--- crates/libraw/src/c_api/init.rs
+++ crates/libraw/src/c_api/init.rs
@@ -8,10 +8,10 @@ impl Libraw {
         Arc::new(ImgdataPointer(unsafe { libraw_sys::libraw_init(0) }))
     }
 
-    fn close(&self) { unsafe { libraw_sys::libraw_close(self.imgdata.0) } }
+    fn _close(&self) { unimplemented!() }
 }
-impl Drop for Libraw {
-    fn drop(&mut self) { self.close(); }
+impl Drop for ImgdataPointer {
+    fn drop(&mut self) { unsafe { libraw_sys::libraw_close(self.0) } }
 }
 impl Default for Libraw {
     fn default() -> Self { Self::new(None) }
