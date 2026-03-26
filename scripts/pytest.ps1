0510dfd029e1a7cc4d2b9529b191fe818b91648a
Glatzel <chenzelin9794@gmail.com>
2026-03-26 12:19:57 +0800

    build: update pytest.ps1 (#370)

--- scripts/pytest.ps1
+++ scripts/pytest.ps1
@@ -1,17 +1,15 @@
 $ROOT = git rev-parse --show-toplevel
 Set-Location $PSScriptRoot/../crates/fornax-py
 $env:PYTHONPATH = Resolve-Path $PSScriptRoot/../crates/fornax-py
-git submodule update --init --recursive
 # run test
 pixi run pytest `
     ./tests `
     -v `
     --durations=10 `
-    --junitxml=tests_report/junit.xml `
+    --junitxml=junit.xml `
     -o junit_family=legacy `
     --cov `
     --cov-report term `
-    --cov-report=xml:tests_report/coverage.xml `
-    --cov-report=html:tests_report/htmlcov `
+    --cov-report=xml:coverage.xml `
     --color=yes
 Set-Location $ROOT
