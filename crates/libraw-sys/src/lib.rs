d1908de34fb5b5c1f14c922e5822f3fc6d0ea0e3
Glatzel <chenzelin9794@gmail.com>
2026-06-27 17:45:20 +0800

    Update lib.rs

--- crates/libraw-sys/src/lib.rs
+++ crates/libraw-sys/src/lib.rs
@@ -8,11 +8,11 @@
     unexpected_cfgs,
     unsafe_code
 )]
-#[cfg(all(not(bindgen), target_os = "windows"))]
+#[cfg(target_os = "windows")]
 include!("bindings-win.rs");
-#[cfg(all(not(bindgen), target_os = "linux", target_arch = "x86_64"))]
+#[cfg(all(target_os = "linux", target_arch = "x86_64"))]
 include!("bindings-linux.rs");
-#[cfg(all(not(bindgen), target_os = "linux", target_arch = "aarch64"))]
+#[cfg(all(target_os = "linux", target_arch = "aarch64"))]
 include!("bindings-linux-aarch64.rs");
-#[cfg(all(not(bindgen), target_os = "macos"))]
+#[cfg(target_os = "macos")]
 include!("bindings-macos.rs");
