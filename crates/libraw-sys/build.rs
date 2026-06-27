9ee4937f35cfebdd3631d567fdca31f210fa9289
Glatzel <chenzelin9794@gmail.com>
2026-06-27 17:43:57 +0800

    f

--- crates/libraw-sys/build.rs
+++ crates/libraw-sys/build.rs
@@ -20,9 +20,7 @@ fn main() {
     //     println!("cargo:rustc-link-lib=m");
     // }
     // generate bindings
-    if env::var("UPDATE").unwrap_or("false".to_string()) == "true"
-        || env::var("BINDGEN").unwrap_or("false".to_string()) == "true"
-    {
+    if env::var("UPDATE_LIBRAW_BINDINGS").unwrap_or("false".to_string()) == "true" {
         let ignored_macros = IgnoreMacros(
             vec![
                 "FP_INFINITE".into(),
@@ -50,42 +48,34 @@ fn main() {
             .generate()
             .unwrap();
 
-        if env::var("UPDATE").unwrap_or("false".to_string()) == "true" {
-            match env::var("CARGO_CFG_TARGET_OS").unwrap().as_str() {
-                "windows" => {
+        match env::var("CARGO_CFG_TARGET_OS").unwrap().as_str() {
+            "windows" => {
+                bindings
+                    .write_to_file("./src/bindings-win.rs")
+                    .expect("Couldn't write bindings!");
+            }
+            "linux" => match env::var("CARGO_CFG_TARGET_ARCH").unwrap().as_str() {
+                "x86_64" => {
                     bindings
-                        .write_to_file("./src/bindings-win.rs")
+                        .write_to_file("./src/bindings-linux.rs")
                         .expect("Couldn't write bindings!");
                 }
-                "linux" => match env::var("CARGO_CFG_TARGET_ARCH").unwrap().as_str() {
-                    "x86_64" => {
-                        bindings
-                            .write_to_file("./src/bindings-linux.rs")
-                            .expect("Couldn't write bindings!");
-                    }
-                    "aarch64" => {
-                        bindings
-                            .write_to_file("./src/bindings-linux-aarch64.rs")
-                            .expect("Couldn't write bindings!");
-                    }
-                    other => {
-                        panic!("Unsupported OS: {other}")
-                    }
-                },
-                "macos" => {
+                "aarch64" => {
                     bindings
-                        .write_to_file("./src/bindings-macos.rs")
+                        .write_to_file("./src/bindings-linux-aarch64.rs")
                         .expect("Couldn't write bindings!");
                 }
                 other => {
                     panic!("Unsupported OS: {other}")
                 }
-            }
-            if env::var("BINDGEN").unwrap_or("false".to_string()) == "true" {
-                println!("cargo:rustc-cfg=bindgen");
+            },
+            "macos" => {
                 bindings
-                    .write_to_file(PathBuf::from(env::var("OUT_DIR").unwrap()).join("bindings.rs"))
+                    .write_to_file("./src/bindings-macos.rs")
                     .expect("Couldn't write bindings!");
+            }
+            other => {
+                panic!("Unsupported OS: {other}")
             }
         }
     }
