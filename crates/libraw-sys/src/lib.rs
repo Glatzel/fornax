f74eb1d940302f273b83b0688ce18b575bc0b57f
Glatzel <chenzelin9794@gmail.com>
2026-08-02 07:13:26 +0800

    Update lib.rs

--- crates/libraw-sys/src/lib.rs
+++ crates/libraw-sys/src/lib.rs
@@ -3,13 +3,13 @@
     clippy::approx_constant,
     clippy::missing_safety_doc,
     clippy::ptr_offset_with_cast,
-    clippy::pub_underscore_fields
+    clippy::pub_underscore_fields,
     clippy::useless_transmute,
     dead_code,
     non_camel_case_types,
     non_snake_case,
     non_upper_case_globals,
-    unexpected_cfgs,
+    unexpected_cfgs
 )]
 #[cfg(target_os = "windows")]
 include!("bindings-win.rs");
